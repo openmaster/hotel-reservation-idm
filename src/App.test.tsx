9215224e309814886eb87076b61c9db46db4f23f
Vishavjeet <vishav4@gmail.com>
2023-08-04 16:36:56 -0400

    initial setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByRole('button', { name: 'me' })
+  expect(linkElement).toBeInTheDocument()
+  fireEvent.click(linkElement)
+  expect(screen.getByText('rocky')).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
+  const [val, setVal] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {val && <p>rocky</p>}
+        <button onClick={() => { setVal(true) }}>me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

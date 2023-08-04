ef632f004efb321f46d80034713b3b0a6b85e85d
Vishavjeet <vishav4@gmail.com>
2023-08-04 16:57:24 -0400

    fixed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App () {
@@ -7,7 +6,6 @@ function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         {val && <p>rocky</p>}
         <button onClick={() => { setVal(true) }}>me</button>
         <a
